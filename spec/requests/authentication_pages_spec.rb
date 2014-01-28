91dd1a98edd396192c0a14023b9e444d3e3c6beb
Melanie Gin <melaniegin1@gmail.com>
2014-01-28 12:00:47 -0800

    Finished chapter 9 exercises

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -53,21 +53,6 @@ describe "Authentication" do
 		describe "for non-signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
 
-			describe "when attempting to visit a protected page" do
-				before do
-					visit edit_user_path(user)
-					fill_in "Email", 	with: user.email
-					fill_in "Password", with: user.password
-					click_button "Sign in"
-				end
-
-				describe "after signing in" do
-					it "should render the desired protected page" do
-						expect(page).to have_title('Edit user')
-					end
-				end
-			end
-
 			describe "in the Users controller" do
 
 				describe "visiting the edit page" do
@@ -86,6 +71,39 @@ describe "Authentication" do
 				end
 
 			end
+
+			it { should_not have_link('Profile', 		href: user_path(user)) }
+			it { should_not have_link('Settings', 		href: edit_user_path(user)) }
+
+			describe "when attempting to visit a protected page" do
+		        before do
+		        	visit edit_user_path(user)
+		          	fill_in "Email",    with: user.email
+		          	fill_in "Password", with: user.password
+		          	click_button "Sign in"
+		        end
+
+		        describe "after signing in" do
+
+		          	it "should render the desired protected page" do
+		            	expect(page).to have_title('Edit user')
+		          	end
+
+		          	describe "when signing in again" do
+		            	before do
+					        signout_path
+					        visit signin_path
+					        fill_in "Email",    with: user.email
+					        fill_in "Password", with: user.password
+					       	click_button "Sign in"
+		            	end
+
+			            it "should render the default (profile) page" do
+			            	expect(page).to have_title(user.name)
+	            		end
+		         	end
+		        end
+		    end
 		end
 
 		describe "as wrong user" do
@@ -101,6 +119,18 @@ describe "Authentication" do
 
 			describe "submitting a PATCH request to the Users#update action" do
 				before { patch user_path(wrong_user) }
+				specify { expect(response).to redirect_to(root_url) }
+			end
+		end
+
+		describe "as non-admin user" do
+			let(:user) { FactoryGirl.create(:user) }
+			let(:non_admin) { FactoryGirl.create(:user) }
+
+			before { sign_in non_admin, no_capybara: true }
+
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(user) }
 				specify { expect(response).to redirect_to(root_url) }
 			end
 		end
